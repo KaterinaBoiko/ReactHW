cbd0ecbe01f0b8a4835f3f8d09f49bff774aaf15
Kateryna Boiko <katerina.boiko16@gmail.com>
2020-06-15 16:38:27 +0300

    5 - list from object array

--- hw1/src/index.js
+++ hw1/src/index.js
@@ -4,7 +4,34 @@ import './index.css';
 //import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const presidents = ["Эндрю Джексон", "Мартин Ван Бюрен", "Уильям Гаррисон"];
+const presidentStrings = ["Эндрю Джексон", "Мартин Ван Бюрен", "Уильям Гаррисон"];
+const presidentObjects = [
+  {
+    firstName: "Джон",
+    lastName: "Тайлер",
+    presidentIndex: 10
+  },
+  {
+    firstName: "Джеймс Нокс",
+    lastName: "Полк",
+    presidentIndex: 11
+  },
+  {
+    firstName: "Закари",
+    lastName: "Тейлор",
+    presidentIndex: 12
+  },
+  {
+    firstName: "Миллард",
+    lastName: "Филлмор",
+    presidentIndex: 13
+  },
+  {
+    firstName: "Франклин",
+    lastName: "Пирс",
+    presidentIndex: 14
+  }
+]
 
 ReactDOM.render(
   <React.StrictMode>
@@ -20,13 +47,26 @@ ReactDOM.render(
     </ol>
     <ul>
       {
-        presidents.map((president) =>
+        presidentStrings.map((president) =>
           <li key={president}>{president}</li>)
+      }
+    </ul>
+    <ul>
+      {
+        presidentObjects
+          .filter((president) =>
+            president.presidentIndex % 2 !== 0)
+          .map((president) =>
+            <li key={president.presidentIndex}>{formatOutput(president)}</li>)
       }
     </ul>
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+function formatOutput({ firstName, lastName, presidentIndex }) {
+  return `${lastName}, ${firstName}, ${presidentIndex}-й`;
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
